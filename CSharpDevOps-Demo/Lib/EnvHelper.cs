d9e6db0fe74296637f3db4b0d41a924f4c1df2c1
Benjamin Bolzmann <bb@truecare365.de>
2021-01-23 23:26:52 +0100

    Init

---
+++ CSharpDevOps-Demo/Lib/EnvHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharpDevOps_Demo.Lib
+{
+    public class EnvHelper
+    {
+        public static string GetEnvironmentVariable(string varName)
+        {
+            varName = varName.ToUpper();
+
+            var processVar = Environment.GetEnvironmentVariable(varName, EnvironmentVariableTarget.Process);
+            if (!string.IsNullOrEmpty(processVar)) return processVar;
+
+            var userVar = Environment.GetEnvironmentVariable(varName, EnvironmentVariableTarget.User);
+            if (!string.IsNullOrEmpty(userVar)) return userVar;
+
+            var machineVar = Environment.GetEnvironmentVariable(varName, EnvironmentVariableTarget.Machine);
+            if (!string.IsNullOrEmpty(machineVar)) return machineVar;
+
+            return string.Empty;
+        }
+    }
+}
